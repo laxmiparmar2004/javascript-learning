dd06126b42d85c78b492876ecef64b730b7b3fbc
laxmiparmar2004 <158476758+laxmiparmar2004@users.noreply.github.com>
2024-06-04 15:36:48 +0000

    datatypes

---
+++ javascriptcodes/codee2.js
@@ -0,0 +1,15 @@
+//window.alert(3+3) it will give error as not supported by nodejs
+//datatypes in javascript
+
+// one more datatype which is symbol is used for uniqueness mostly in react to be learnt later
+// REMEMBER the typeof null is object and typeof undefinied is undefined
+let num=123;
+let name="laxmi";
+let ans=true;
+let a=null;
+let b;
+console.log(typeof num)
+console.log(typeof ans);
+console.log(typeof a)
+console.log(typeof b)
+console.table([typeof num,typeof ans,typeof a,typeof b])
